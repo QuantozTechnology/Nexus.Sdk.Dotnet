d833037d7526a4da9b21f9b1379f79336dcaba9d
mohsinr <mohsin@sebta.com>
2024-10-15 11:41:37 +0200

    Update TokenOperationResponse to add Fiat Amounts

--- Nexus.Sdk.Token/Responses/TokenOperationResponse.cs
+++ Nexus.Sdk.Token/Responses/TokenOperationResponse.cs
@@ -5,7 +5,7 @@ namespace Nexus.Sdk.Token.Responses
     public record TokenOperationResponse
     {
         [JsonConstructor]
-        public TokenOperationResponse(string code, string hash, OperationAccountResponses senderAccount, OperationAccountResponses receiverAccount, decimal amount, string created, string? finished, string status, string type, string? memo, string? message, string cryptoCode, string tokenCode, string paymentReference)
+        public TokenOperationResponse(string code, string hash, OperationAccountResponses senderAccount, OperationAccountResponses receiverAccount, decimal amount, string created, string? finished, string status, string type, string? memo, string? message, string cryptoCode, string tokenCode, string paymentReference, decimal fiatAmount, decimal netFiatAmount)
         {
             Code = code;
             Hash = hash;
@@ -21,6 +21,8 @@ namespace Nexus.Sdk.Token.Responses
             CryptoCode = cryptoCode;
             TokenCode = tokenCode;
             PaymentReference = paymentReference;
+            FiatAmount = fiatAmount;
+            NetFiatAmount = netFiatAmount;
         }
 
         [JsonPropertyName("code")]
@@ -64,5 +66,11 @@ namespace Nexus.Sdk.Token.Responses
 
         [JsonPropertyName("paymentReference")]
         public string PaymentReference { get; set; }
+
+        [JsonPropertyName("fiatAmount")]
+        public decimal FiatAmount { get; private set; }
+
+        [JsonPropertyName("netFiatAmount")]
+        public decimal NetFiatAmount { get; private set; }
     }
 }
