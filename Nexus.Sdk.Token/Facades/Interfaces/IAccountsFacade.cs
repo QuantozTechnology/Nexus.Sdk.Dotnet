c71bcd354864a421151da68696e1140b5dcd22e3
Wayne Robért <wayne@quantoz.com>
2024-10-22 14:19:44 +0200

    QPAY-63: Updated method docs

--- Nexus.Sdk.Token/Facades/Interfaces/IAccountsFacade.cs
+++ Nexus.Sdk.Token/Facades/Interfaces/IAccountsFacade.cs
@@ -98,7 +98,8 @@ public interface IAccountsFacade
     public Task<SignableResponse> Update(string customerCode, string accountCode, UpdateTokenAccountRequest updateRequest, string? customerIPAddress = null);
 
     /// <summary>
-    /// Delete an account
+    /// Deletes an account.
+    /// Please note that all token balances needs to be 0 and all tokens disabled.
     /// </summary>
     /// <param name="accountCode">{crypto}-{publickey} combination of the account. E.g. XLM-GAW6GBLA5U4KCXV4E5SZTVERBF3AUASEPNTN4ZXSXLCROOTJ7KQQW4S7</param>
     public Task<NexusResponse> DeleteAccount(string accountCode);
