77f96b6b14eba3fe54c0f06a8d675d6a4064d998
Martin van Niekerk <martin@quantoz.com>
2025-08-05 14:41:37 +0200

    Refactor FilePath property in DocumentRequest to include URL encoding (#76)

--- Nexus.Sdk.Token/Requests/DocumentRequest.cs
+++ Nexus.Sdk.Token/Requests/DocumentRequest.cs
@@ -5,14 +5,23 @@ namespace Nexus.Sdk.Token.Requests;
 
 public class DocumentRequest
 {
+    private string _filePath = string.Empty;
+
     /// <summary>
     /// The path of the file in the Document Store
     /// </summary>
     /// <example>
     /// path/to/file.txt
     /// </example>
+    /// <remarks>
+    /// The path is automatically URL-encoded to ensure it is safe for transmission
+    /// </remarks>
     [JsonPropertyName("filePath")]
     [Required]
     [StringLength(255)]
-    public required string FilePath { get; set; }
+    public required string FilePath
+    {
+        get => _filePath;
+        set => _filePath = Uri.EscapeDataString(value);
+    }
 }
